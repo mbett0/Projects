2e3d1d7424534c89b13db61520d6f912e3e89cb9
mbett0 <141190310+mbett0@users.noreply.github.com>
2023-08-01 17:04:31 -0700

    Add files via upload

---
+++ ComputerGraphics_C++/hw-8/hw-8/part2/raster.cpp
@@ -0,0 +1,9 @@
+#include <vector>
+#include <string>
+#include <iostream>
+#include "common.h"
+
+void Rasterize(Pixel* pixels, int width, int height, const std::vector<Triangle>& tris)
+{
+    std::cout<<"TODO: implement rasterization"<<std::endl;
+}
